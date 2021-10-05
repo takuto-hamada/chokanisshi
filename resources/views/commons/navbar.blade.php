5ce61374580f94167ac95edb1200a5d51dca83f5
takuto.hamada <ta_910@icloud.com>
2021-10-05 08:03:46 +0000

    top page

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,20 @@
+<header class="mb-4">
+    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+        
+        <a class="navbar-brand" href="/">釣果日誌</a>
+
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="nav-bar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav">
+                
+                <li class="nav-item"><a href="#" class="nav-link">新規登録</a></li>
+                
+                <li class="nav-item"><a href="#" class="nav-link">ログイン</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
