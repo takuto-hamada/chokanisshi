3eec58c7d03c448e3fe77b87aba827d471ad2fc1
takuto.hamada <ta_910@icloud.com>
2021-10-05 08:54:51 +0000

    user registration

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -11,7 +11,7 @@
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
                 
-                <li class="nav-item"><a href="#" class="nav-link">新規登録</a></li>
+                <li>{!! link_to_route('signup.get', '新規会員登録', [], ['class' => 'nav-link']) !!}</li>
                 
                 <li class="nav-item"><a href="#" class="nav-link">ログイン</a></li>
             </ul>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,5 +3,7 @@
 @section('content')
     <div class="text-center">
         <h1>釣果日誌</h1>
+        
+        {!! link_to_route('signup.get', 'アカウントをお持ちでない方はこちら', [], ['class' => 'btn btn-lg btn-dark']) !!}
     </div>
 @endsection
